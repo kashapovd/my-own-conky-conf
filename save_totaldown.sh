85815178b7207b026f03d704abc80fe21200fa79
kashapovd <y.kashapovd@gmail.com>
2020-01-04 09:03:40 +0700

    update

--- save_totaldown.sh
+++ save_totaldown.sh
@@ -3,4 +3,4 @@
 currd=`cat /sys/class/net/wlp4s0/statistics/rx_bytes`
 prevd=`cat /home/yaroslav/projects/repos/my-own-conky-conf/t_prevsn`
 sumd=$(($currd + $prevd))
-echo $sumd > $dir/t_prevsn
+echo $sumd > /home/yaroslav/projects/repos/my-own-conky-conf/t_prevsn
